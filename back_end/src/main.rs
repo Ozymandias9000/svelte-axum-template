decb016b104b20a722d3ddf9265cdf3f9681134e
Ozymandias9000 <nick_murphy@sbcglobal.net>
2023-01-28 17:26:31 -0500

    use .env

--- back_end/src/main.rs
+++ back_end/src/main.rs
@@ -5,12 +5,13 @@
 
 use axum::Router;
 use axum_sessions::{async_session::MemoryStore, SessionLayer};
-use dotenv::dotenv;
+use dotenvy::dotenv;
 use std::net::SocketAddr;
 use std::{env, sync::Arc};
 use tracing::log::warn;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+pub mod db;
 pub mod middlewares;
 pub mod routes;
 mod services;
@@ -25,23 +26,22 @@ mod store;
 async fn main() {
     dotenv().ok();
 
+    let (port, host, secret, log_level, token) = from_env();
+
     // start tracing - level set by either RUST_LOG env variable or defaults to debug
     tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "svelte_axum_project=debug".into()),
-        ))
+        .with(tracing_subscriber::EnvFilter::new(log_level))
         .with(tracing_subscriber::fmt::layer())
         .init();
 
     // configure server from environmental variables
-    let (port, host, secret) = from_env();
 
     let addr: SocketAddr = format!("{}:{}", host, port)
         .parse()
         .expect("Can not parse address and port");
 
     // create store for backend.  Stores an api_token.
-    let shared_state = Arc::new(store::Store::new("123456789"));
+    let shared_state = Arc::new(store::Store::new(&token));
 
     let session_cookie = env::var("SESSION_COOKIE_NAME").ok().unwrap();
     // setup up sessions and store to keep track of session information
@@ -53,7 +53,7 @@ async fn main() {
         .merge(services::front_public_route())
         .merge(services::backend(session_layer, shared_state));
 
-    tracing::info!("listening on http://{}", addr);
+    tracing::info!("🚀 listening on http://{}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -73,16 +73,24 @@ async fn shutdown_signal() {
 
 // Variables from Environment or default to configure server
 // port, host, secret
-fn from_env() -> (String, String, String) {
+fn from_env() -> (String, String, String, String, String) {
     if env::var("SERVER_SECRET").is_err() {
         warn!("env var SERVER_SECRET should be set and unique (64 bytes long)");
+    }
+    if env::var("API_TOKEN").is_err() {
+        warn!("env var API_TOKEN should be set and unique (64 bytes long)");
     }
     (
         env::var("SERVER_PORT").ok().unwrap(),
         env::var("SERVER_HOST").ok().unwrap(),
         env::var("SERVER_SECRET").ok().unwrap_or_else(|| {
             "this needs to be 64bytes. recommended that you set Secret instead of fixed value"
-                .to_string()
+                .into()
+        }),
+        env::var("RUST_LOG").unwrap_or_else(|_| "svelte_axum_project=debug".into()),
+        env::var("API_TOKEN").ok().unwrap_or_else(|| {
+            "this needs to be 64bytes. recommended that you set Secret instead of fixed value"
+                .into()
         }),
     )
 }
